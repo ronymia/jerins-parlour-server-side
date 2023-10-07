313221dc70372d698b84951a79c38a2d29c26094
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-10-07 23:17:51 +0600

    service api

--- index.js
+++ index.js
@@ -28,13 +28,20 @@ async function run() {
           // Connect the client to the server	(optional starting in v4.7)
           await client.connect();
 
+          const serviceCollection = client.db("jerins_parlour").collection("services");
+
+          // all apis start here
+          app.get("/services", async (req, res) => {
+               const services = await serviceCollection.find().toArray();
+               res.send(services);
+          })
 
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
      } finally {
           // Ensures that the client will close when you finish/error
-          await client.close();
+          // await client.close();
      }
 }
 run().catch(console.dir);
